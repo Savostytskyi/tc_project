3fd3efa2ed1ed72033f4985662d1edae199f0a21
Anton Savostytskyi <anton_savostytskyi@epam.com>
2018-01-16 16:02:00 +0100

    minor fix

--- backend/src/test/java/com/epam/qa/runner/BackendTestRunner.java
+++ backend/src/test/java/com/epam/qa/runner/BackendTestRunner.java
@@ -3,7 +3,7 @@ package com.epam.qa.runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "classpath:features/",
+@CucumberOptions(features = "classpath:features",
         glue = {"com/epam/qa/stepdefs/"})
 public class BackendTestRunner extends AbstractTestNGCucumberTests {
 
