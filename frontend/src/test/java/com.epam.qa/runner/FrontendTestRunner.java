b31d78f495e888c3ea801294a0556925c43b5811
Anton Savostytskyi <anton_savostytskyi@epam.com>
2018-01-17 19:49:25 +0100

    features file path fixed

--- frontend/src/test/java/com.epam.qa/runner/FrontendTestRunner.java
+++ frontend/src/test/java/com.epam.qa/runner/FrontendTestRunner.java
@@ -3,7 +3,7 @@ package com.epam.qa.runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "classpath:features/menu",
+@CucumberOptions(features = "classpath:features",
         glue = {"com.epam.qa.stepdefs"})
 public class FrontendTestRunner extends AbstractTestNGCucumberTests {
 }
